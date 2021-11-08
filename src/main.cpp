1986d691b6d822d539d1f25fbfff92dad26f4376
Felipe Balbi <felipe@balbi.sh>
2021-11-08 18:22:36 +0200

    Start collecting information
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,58 @@
  */
 #include <argparse/argparse.hpp>
 #include <chrono>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <ranges>
 #include <thread>
 #include <utility>
+#include <regex>
 
 static auto collect_data(int interval, int num_samples)
 {
+    const std::regex process_regex("([0-9]+)");
+    std::smatch process_match;
+
     for (int i = 0; i < num_samples; ++i) {
-        std::cout << "Thread 1 executing\n";
+        const std::filesystem::path procfs { "/proc" };
+
+        for (auto const& entry :
+            std::filesystem::directory_iterator { procfs }) {
+            std::string fname = entry.path().filename().string();
+
+            if (!entry.is_directory())
+                continue;
+
+            if (!std::regex_match(fname, process_match, process_regex))
+                continue;
+
+            std::ifstream process_name;
+            std::ifstream process_pss;
+
+            process_name.open(entry.path() / "cmdline");
+            if (!process_name.is_open())
+                continue;
+
+            process_pss.open(entry.path() / "smaps_rollup");
+            if (!process_pss.is_open())
+                continue;
+
+            std::string cmdline;
+            std::string pss;
+
+            getline(process_name, cmdline);
+
+            if (cmdline.empty())
+                continue;
+
+            getline(process_pss, pss);
+            getline(process_pss, pss);
+            getline(process_pss, pss);
+
+            std::cout << fname << ": " << cmdline << ", " << pss << std::endl;
+        }
+
         std::this_thread::sleep_for(std::chrono::seconds(interval));
     }
 }
